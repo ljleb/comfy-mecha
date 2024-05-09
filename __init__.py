a93d5f86685c9c420bdac400b46bde9df67045b9
ljleb <lebel.louisjacob@gmail.com>
2024-05-09 16:04:44 -0400

    always update main reqs

--- __init__.py
+++ __init__.py
@@ -4,43 +4,43 @@ import subprocess
 import sys
 import threading
 
-try:
-    import sd_mecha
-except ImportError:
-    def handle_stream(stream, prefix):
-        stream.reconfigure(encoding=locale.getpreferredencoding(), errors='replace')
-        for msg in stream:
-            if prefix == '[!]' and ('it/s]' in msg or 's/it]' in msg) and ('%|' in msg or 'it [' in msg):
-                if msg.startswith('100%'):
-                    print('\r' + msg, end="", file=sys.stderr),
-                else:
-                    print('\r' + msg[:-1], end="", file=sys.stderr),
+
+def handle_stream(stream, prefix):
+    stream.reconfigure(encoding=locale.getpreferredencoding(), errors='replace')
+    for msg in stream:
+        if prefix == '[!]' and ('it/s]' in msg or 's/it]' in msg) and ('%|' in msg or 'it [' in msg):
+            if msg.startswith('100%'):
+                print('\r' + msg, end="", file=sys.stderr),
             else:
-                if prefix == '[!]':
-                    print(prefix, msg, end="", file=sys.stderr)
-                else:
-                    print(prefix, msg, end="")
+                print('\r' + msg[:-1], end="", file=sys.stderr),
+        else:
+            if prefix == '[!]':
+                print(prefix, msg, end="", file=sys.stderr)
+            else:
+                print(prefix, msg, end="")
 
 
-    def run_script(cmd, cwd='.'):
-        if len(cmd) > 0 and cmd[0].startswith("#"):
-            print(f"[Mecha] Unexpected behavior: `{cmd}`")
-            return 0
+def run_script(cmd, cwd='.'):
+    if len(cmd) > 0 and cmd[0].startswith("#"):
+        print(f"[Mecha] Unexpected behavior: `{cmd}`")
+        return 0
 
-        process = subprocess.Popen(cmd, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, bufsize=1)
+    process = subprocess.Popen(cmd, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, bufsize=1)
 
-        stdout_thread = threading.Thread(target=handle_stream, args=(process.stdout, ""))
-        stderr_thread = threading.Thread(target=handle_stream, args=(process.stderr, "[!]"))
+    stdout_thread = threading.Thread(target=handle_stream, args=(process.stdout, ""))
+    stderr_thread = threading.Thread(target=handle_stream, args=(process.stderr, "[!]"))
 
-        stdout_thread.start()
-        stderr_thread.start()
+    stdout_thread.start()
+    stderr_thread.start()
 
-        stdout_thread.join()
-        stderr_thread.join()
+    stdout_thread.join()
+    stderr_thread.join()
 
-        return process.wait()
+    return process.wait()
 
-    requirements_path = pathlib.Path(__file__).parent / "requirements.txt"
-    run_script([sys.executable, '-s', '-m', 'pip', 'install', '-r', str(requirements_path)])
+
+requirements_path = pathlib.Path(__file__).parent / "requirements.txt"
+run_script([sys.executable, '-s', '-m', 'pip', 'install', '-r', str(requirements_path)])
+
 
 from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
