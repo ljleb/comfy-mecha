d8f0d88133ff4a989a04213508f9282c3980f2a3
ljleb <lebel.louisjacob@gmail.com>
2025-02-03 22:40:57 -0500

    update

--- nodes/__init__.py
+++ nodes/__init__.py
@@ -3,9 +3,9 @@ from . import parameter_nodes
 
 NODE_CLASS_MAPPINGS = (
     merge_nodes.NODE_CLASS_MAPPINGS |
-    hyper_nodes.NODE_CLASS_MAPPINGS
+    parameter_nodes.NODE_CLASS_MAPPINGS
 )
 NODE_DISPLAY_NAME_MAPPINGS = (
     merge_nodes.NODE_DISPLAY_NAME_MAPPINGS |
-    hyper_nodes.NODE_DISPLAY_NAME_MAPPINGS
+    parameter_nodes.NODE_DISPLAY_NAME_MAPPINGS
 )
